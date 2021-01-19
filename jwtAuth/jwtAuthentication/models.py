76c280e5fbc2f2e32fa3d9f3c7fdc3f61d633d98
Rohit Yadav <59415152+rohityadav321@users.noreply.github.com>
2021-01-19 15:15:52 +0530

    added relations to the model

--- jwtAuth/jwtAuthentication/models.py
+++ jwtAuth/jwtAuthentication/models.py
@@ -2,7 +2,26 @@ from django.db import models
 
 
 # Create your models here.
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class User(models.Model):
     name = models.CharField(max_length=50)
     contact = models.IntegerField()
-    email = models.EmailField(max_length=50)
+    role = models.ForeignKey(
+        Role, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ManyToManyField(User, null=True)
+
+    def __str__(self):
+        return self.name
